398cc6407d4bac3090228437b25e60a9ae8003b9
maironald <ronald.mai.goldenowl@gmail.com>
2024-01-16 09:24:26 +0700

    test-deploy-in-render

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
     include Visible
+    # depedent is the helper that can help you to determine the relationship (when article is destroy, it will destroy the comments too)
     has_many :comments, dependent: :destroy
 
-    validates :title, presence: true
+    # it will check the title and body is not nil (if they are nil, the program will raise an error)
+    # the length will check the body is at least 10. (if it is less than 10, the program will raise an error)
+    validates :title, presence: true 
     validates :body, presence: true, length: {minimum: 10}
 end
